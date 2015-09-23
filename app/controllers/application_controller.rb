37df6a9e812b4153c95f4bd78e699bdde4e56a6b
luoping <luoping0425@gmail.com>
2015-09-23 22:45:38 +0800

    FEATURE - register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,42 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def bounce!(message, url = root_url)
+    flash[:notice] = message if message
+    if request.xhr?
+      render(:update) {|page| page.redirect_to(url)}
+    else
+      redirect_to url
+    end
+  end
+
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user=(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def logged_in?
+    current_user
+  end
+
+  def login!(user)
+    p ' **' * 20
+    p user.inspect
+    if user
+      session[:user_id] = user.id
+      @current_user = user
+    end
+  end
+
+  def require_login
+    session[:return_to] = request.fullpath
+    bounce! 'Sorry, you must login first' unless current_user
+  end
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      flash[:notice] = 'User create successfully'
+      redirect_to root_path
+    else
+      falsh[:alert] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
